df38ffc752131144ca1f86b6dba47b1e7eff0bf3
ArneBoettger <77060523+SeppReifen@users.noreply.github.com>
2021-08-30 14:00:34 +0200

    configurable file paths

--- src/main/java/org/ab/HL7FileToSqlRouteBuilder.java
+++ src/main/java/org/ab/HL7FileToSqlRouteBuilder.java
@@ -7,9 +7,9 @@ public class HL7FileToSqlRouteBuilder extends RouteBuilder {
     private static Hl7ToStringToSqlProcessor processor = new Hl7ToStringToSqlProcessor();
 
     @Override
-    public void configure() throws Exception {
+    public void configure() {
 
-        from("file:C:/messages/newMessages?move=C:/messages/archive/${file:name}")
+        from("file:{{file.input}}?move={{file.archive}}/${file:name}")
                 .log("Reading patient information from hl7 file and prepare sql insert information")
                 .process(processor)
                 .log("Send sql insert")
